eadfc3a326dd548fffeebf79875ba81e0bfc3491
Santi Romero <santiagooromero1234@gmail.com>
2022-10-17 14:38:08 -0300

    Evaluation functions

--- evaluation.py
+++ evaluation.py
@@ -3,35 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import metrics
-from sklearn.preprocessing import label_binarize
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_validate
+import time
 
-
-def get_performance(predictions, y_test, labels=[1, 0]):
-    accuracy = metrics.accuracy_score(y_test, predictions)
-    precision = metrics.precision_score(y_test, predictions, average='micro')
-    recall = metrics.recall_score(y_test, predictions, average='micro')
-    f1_score = metrics.f1_score(y_test, predictions, average='micro')
-    
-    report = metrics.classification_report(y_test, predictions)
-    
-    cm = metrics.confusion_matrix(y_test, predictions)
-    cm_as_dataframe = pd.DataFrame(data=cm)
-    
-    print('Model Performance metrics:')
-    print('-'*30)
-    print('Accuracy:', accuracy)
-    print('Precision:', precision)
-    print('Recall:', recall)
-    print('F1 Score:', f1_score)
-    print('\nModel Classification report:')
-    print('-'*30)
-    print(report)
-    print('\nPrediction Confusion Matrix:')
-    print('-'*30)
-    print(cm_as_dataframe, "\n")
-    
-    return accuracy, precision, recall, f1_score
-
+#---------------------------------------------MODEL EVALUATION---------------------------------------------#
 
 """
 Approch to compute AUC score of multi class classification problem:
@@ -44,6 +20,8 @@ A binary classifier is then trained on each binary classification problem and pr
 
 One vs One Technique : It is really similar to OvR, but instead of comparing each class with the rest,
 we compare all possible two-class combinations of the dataset.
+
+We will be using Ons vs All Technique as it is a little bit "friendlier" to plot (less images) and is good enough for our purposes
 """ 
 
 #AUXILIARIES
@@ -60,7 +38,6 @@ def calculate_tpr_fpr(y_real, y_pred):
         tpr: The True Positive Rate of the classifier
         fpr: The False Positive Rate of the classifier
     '''
-    #print("como")
     # Calculates the confusion matrix and recover each element
     cm = metrics.confusion_matrix(y_real, y_pred)
     TN = cm[0, 0]
@@ -86,7 +63,6 @@ def get_all_roc_coordinates(y_real, y_proba):
         tpr_list: The list of TPRs representing each threshold.
         fpr_list: The list of FPRs representing each threshold.
     '''
-    #print("hola")
     tpr_list = [0]
     fpr_list = [0]
     for i in range(len(y_proba)):
@@ -106,7 +82,6 @@ def plot_roc_curve(tpr, fpr, scatter = True, ax = None):
         fpr: The list of FPRs representing each coordinate.
         scatter: When True, the points used on the calculation will be plotted with the line (default = True).
     '''
-    #print("estas")
     if ax == None:
         plt.figure(figsize = (5, 5))
         ax = plt.axes()
@@ -137,7 +112,6 @@ def plot_roc_auc_score_multiclass_ovr(model, X_test, y_test):
 
         # Gets the class
         c = classes[i]
-        print("Class:", c)
         
         # Prepares an auxiliar dataframe to help with the plots
         y_proba = model.predict_proba(X_test)
@@ -161,7 +135,6 @@ def plot_roc_auc_score_multiclass_ovr(model, X_test, y_test):
         ax_bottom.set_title("ROC Curve OvR")
 
         # Calculates the ROC AUC OvR
-        
         roc_auc_ovr[c] = metrics.roc_auc_score(df_aux['class'], df_aux['prob'])
 
     plt.tight_layout()
@@ -177,72 +150,79 @@ def plot_roc_auc_score_multiclass_ovr(model, X_test, y_test):
 
     ans = avg_roc_auc/i
     print(f"Average ROC AUC OvR: {ans:.4f}")
-    return ans
+ 
+def get_avg_roc_auc(model, X_test, y_test):
 
-
-"""
-#ONE VS ONE
-
-def plot_roc_auc_multiclass_ovo(model, X_test, y_test):
-
-    #Get Classes Combinations
+    #Neccesaries Tranformations
+    y_test = y_test["Rating"].values.tolist()
     classes = model.classes_
-    classes_combinations = []
-    class_list = list(classes)
-    for i in range(len(class_list)):
-        for j in range(i+1, len(class_list)):
-            classes_combinations.append([class_list[i], class_list[j]])
-            classes_combinations.append([class_list[j], class_list[i]])
-    
-    # Plots the Probability Distributions and the ROC Curves One vs One
-    plt.figure(figsize = (20, 7))
-    bins = [i/20 for i in range(20)] + [1]
-    roc_auc_ovo = {}
-    for i in range(len(classes_combinations)):
+    roc_auc_ovr = {}
+    for i in range(len(classes)):
+
         # Gets the class
-        comb = classes_combinations[i]
-        c1 = comb[0]
-        c2 = comb[1]
-        c1_index = class_list.index(c1)
-        title = c1 + " vs " +c2
+        c = classes[i]
         
         # Prepares an auxiliar dataframe to help with the plots
         y_proba = model.predict_proba(X_test)
-        df_aux = X_test.copy()
-        df_aux['class'] = y_test
-        df_aux['prob'] = y_proba[:, c1_index]
-        
-        # Slices only the subset with both classes
-        df_aux = df_aux[(df_aux['class'] == c1) | (df_aux['class'] == c2)]
-        df_aux['class'] = [1 if y == c1 else 0 for y in df_aux['class']]
+        df_aux = pd.DataFrame()
+        df_aux['class'] = [1 if y == c else 0 for y in y_test]
+        df_aux['prob'] = y_proba[:, i]
         df_aux = df_aux.reset_index(drop = True)
-        
-        # Plots the probability distribution for the class and the rest
-        ax = plt.subplot(2, 6, i+1)
-        sns.histplot(x = "prob", data = df_aux, hue = 'class', color = 'b', ax = ax, bins = bins)
-        ax.set_title(title)
-        ax.legend([f"Class 1: {c1}", f"Class 0: {c2}"])
-        ax.set_xlabel(f"P(x = {c1})")
-        
-        # Calculates the ROC Coordinates and plots the ROC Curves
-        ax_bottom = plt.subplot(2, 6, i+7)
-        tpr, fpr = metrics.get_all_roc_coordinates(df_aux['class'], df_aux['prob'])
-        metrics.plot_roc_curve(tpr, fpr, scatter = False, ax = ax_bottom)
-        ax_bottom.set_title("ROC Curve OvO")
-        
-        # Calculates the ROC AUC OvO
-        roc_auc_ovo[title] = metrics.roc_auc_score(df_aux['class'], df_aux['prob'])
-    plt.tight_layout()
-
-    #Let´s also get the Roc-Auc value of the different classes
+        roc_auc_ovr[c] = metrics.roc_auc_score(df_aux['class'], df_aux['prob'])
+    
+    # Displays the ROC AUC for each class
     avg_roc_auc = 0
     i = 0
-    for k in roc_auc_ovo:
-        avg_roc_auc += roc_auc_ovo[k]
+    for k in roc_auc_ovr:
+        avg_roc_auc += roc_auc_ovr[k]
         i += 1
-        print(f"{k} ROC AUC OvO: {roc_auc_ovo[k]:.4f}")
-    print(f"average ROC AUC OvO: {avg_roc_auc/i:.4f}")
-"""
 
+    ans = avg_roc_auc/i
+    return ans
 
+#---------------------------------------------LEARNING CURVES AND TUNNING---------------------------------------------#
 
+#Auxiliaries functions without GS
+
+def calculating_error(dict, key_error):
+    error_array = -dict[str(key_error)]
+    error = np.mean(error_array)
+    return error
+
+def cv_calculus(pipe, X_train, y_train, scoring, cv):
+
+    #Perform the CV
+    cv_results = cross_validate(pipe, X_train, y_train, scoring=scoring, cv=cv, return_train_score=True) #return_train_score=True remains harcoded
+
+    #Clean CV Results
+    error_cv = calculating_error(cv_results, "test_score")
+    error_tr = calculating_error(cv_results, "train_score")
+    fit_score_time = (cv_results["fit_time"].mean()) + (cv_results["score_time"].mean())
+
+    #Print the Main results
+    print(f"Training Log Loss Error {error_tr:0.2f}, CV Log Loss Error {error_cv:0.2f}")
+    print('Train/Validation: {}'.format(round(error_cv/error_tr, 1)))
+
+    return error_cv, error_tr, fit_score_time
+
+#Auxiliary functions when GridSearch is performed
+
+def get_errors(grid_results):
+    results = pd.DataFrame.from_dict(grid_results)
+    results = results.sort_values(by=["mean_test_score"], ascending=False)
+    results = results.head().reset_index()
+    error_cv = -results.loc[0, "mean_test_score"]
+    error_tr = -results.loc[0, "mean_train_score"]
+    fit_score_time = (results.loc[0,"mean_fit_time"]) + (results.loc[0,"mean_score_time"])
+    print(f"Training Log Loss {error_tr:0.2f}, CV Log Loss {error_cv:0.2f}")
+    print('Train/Validation: {}'.format(round(error_cv/error_tr, 1)))
+    return error_cv, error_tr, fit_score_time
+
+def grid_search(estimator, parameters, cv, scoring, X_train, y_train):
+    grid = GridSearchCV(estimator, param_grid=parameters, cv=cv, scoring=str(scoring), return_train_score=True)
+    inicio = time.time()
+    grid.fit(X_train, y_train)
+    fin = time.time()
+    print("The time it takes to fit the model is",round(fin-inicio),"seconds.")
+    print("Best params: "+str(grid.best_params_))
+    return grid
